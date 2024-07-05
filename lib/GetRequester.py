3c4ea6468a2f627147ccdec27b8451336badc0dd
Joy Omonya <joynandako1@gmail.com>
2024-07-06 00:02:37 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,6 +8,12 @@ class GetRequester:
 
     def get_response_body(self):
         pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
         pass
+        response_body= self.get_response_body()
+        return requests.json.loads(response_body)
+
+        
